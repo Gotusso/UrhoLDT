0d8939d5f9eb5b809ae7a23bfc6ddd86ffff32d1
Fran <gotusso@gmail.com>
2014-05-30 21:32:32 +0200

    Fixes #1 Autogenerate the index of tolua++ packages to use

--- GenerateAPI.bat
+++ GenerateAPI.bat
@@ -4,12 +4,13 @@ if "%~1"=="" (
 	echo Usage: %~0 URHO_3D_SOURCE_DIR
 	exit /B 1
 )
+echo Generating API...
 if exist out rmdir /s /q out
 mkdir out
 mkdir out\api
 pushd %~1\Source\Engine\LuaScript\pkgs\
-echo Generating API...
-..\..\..\..\Bin\tolua++ -L %~dp0\src\generator.lua -P -o %~dp0\out\api\api.lua Urho3D.tolua
+for %%i in (*.pkg) do echo $pfile "%%~nxi" >> %~dp0\out\Urho3D.pkg
+..\..\..\..\Bin\tolua++ -L %~dp0\src\generator.lua -P -o %~dp0\out\api\api.lua %~dp0\out\Urho3D.pkg
 if errorlevel 1 (
 	popd
 	exit /B 1
@@ -17,5 +18,6 @@ if errorlevel 1 (
 popd
 pushd out
 del api\api.lua
+del api\Urho3D.pkg
 lua ..\lib\luadocumentor-0.1.3\luadocumentor.lua api
 popd
--- GenerateAPI.sh
+++ GenerateAPI.sh
@@ -3,6 +3,7 @@ if [ -z "$1" ]; then
     echo Usage: $0 URHO_3D_SOURCE_DIR
     exit 1
 fi
+echo "Generating API..."
 cd $( dirname $0 )
 WD=`pwd`
 if [ -d "out" ]; then
@@ -10,8 +11,8 @@ if [ -d "out" ]; then
 fi
 mkdir -p out/api
 pushd $1/Source/Engine/LuaScript/pkgs > /dev/null
-echo "Generating API..."
-../../../../Bin/tolua++ -L $WD/src/generator.lua -P -o $WD/out/api/api.lua Urho3D.tolua
+for i in *.pkg; do echo "\$pfile \"$i\"" >> $WD/out/Urho3D.pkg; done
+../../../../Bin/tolua++ -L $WD/src/generator.lua -P -o $WD/out/api/api.lua $WD/out/Urho3D.pkg
 if [ $? -ne 0 ]; then 
 	popd > /dev/null
 	exit 1
@@ -19,5 +20,6 @@ fi
 popd > /dev/null
 pushd out > /dev/null
 rm -f api/api.lua
+rm -f /Urho3D.pkg
 lua ..\lib\luadocumentor-0.1.3\luadocumentor.lua api
 popd > /dev/null
