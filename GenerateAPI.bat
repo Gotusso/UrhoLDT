4a3204d8c9743be9a40492fdf8b64e23a7e0023e
Fran <gotusso@gmail.com>
2014-05-31 17:24:01 +0200

    - Take out the global header to an independent file - Remove unnecessary functions by taking them from the original Urho's file

--- GenerateAPI.bat
+++ GenerateAPI.bat
@@ -8,6 +8,7 @@ echo Generating API...
 if exist out rmdir /s /q out
 mkdir out
 mkdir out\api
+copy src\global.lua out\api\
 pushd %~1\Source\Engine\LuaScript\pkgs\
 for %%i in (*.pkg) do echo $pfile "%%~nxi" >> %~dp0\out\Urho3D.pkg
 ..\..\..\..\Bin\tolua++ -L %~dp0\src\generator.lua -P -o %~dp0\out\api\api.lua %~dp0\out\Urho3D.pkg
@@ -18,6 +19,6 @@ if errorlevel 1 (
 popd
 pushd out
 del api\api.lua
-del api\Urho3D.pkg
+del Urho3D.pkg
 lua ..\lib\luadocumentor-0.1.3\luadocumentor.lua api
 popd
--- GenerateAPI.sh
+++ GenerateAPI.sh
@@ -10,6 +10,7 @@ if [ -d "out" ]; then
   rm -rf out
 fi
 mkdir -p out/api
+cp src/global.lua out/api/
 pushd $1/Source/Engine/LuaScript/pkgs > /dev/null
 for i in *.pkg; do echo "\$pfile \"$i\"" >> $WD/out/Urho3D.pkg; done
 ../../../../Bin/tolua++ -L $WD/src/generator.lua -P -o $WD/out/api/api.lua $WD/out/Urho3D.pkg
@@ -20,6 +21,6 @@ fi
 popd > /dev/null
 pushd out > /dev/null
 rm -f api/api.lua
-rm -f /Urho3D.pkg
+rm -f Urho3D.pkg
 lua ../lib/luadocumentor-0.1.3/luadocumentor.lua api
 popd > /dev/null
